2aae04eca38fbe310f85eba55e3771a135351bb1
Nitesh_ranjan <nitesh.mohanty@caprusit.com>
2024-09-23 11:59:09 +0530

    added useContext

--- routes/quiz.ts
+++ routes/quiz.ts
@@ -197,4 +197,18 @@ router.get('/resources/react/hooks/effect', (req, res) => {
   });
 });
 
+router.get('/resources/react/hooks/useContext', (req, res) => {
+  // Read the Markdown file
+  const filePath = path.join(__dirname, "../resources/reactUseContext.md");
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading markdown file');
+    }
+    // Convert Markdown to HTML
+    const htmlContent = marked(data);
+    // Render EJS template
+    res.render('template', { content: htmlContent });
+  });
+});
+
 export default router;
