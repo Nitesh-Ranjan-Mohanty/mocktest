e2449e8a040990aef43652d819737d63edc5120c
OD!N <126156869+Nitesh-Ranjan-Mohanty@users.noreply.github.com>
2024-10-07 11:54:59 +0530

    Update quiz.ts
    sdkman

--- routes/quiz.ts
+++ routes/quiz.ts
@@ -211,4 +211,18 @@ router.get('/resources/react/hooks/useContext', (req, res) => {
   });
 });
 
+router.get('/resources/java/sdkman', (req, res) => {
+  // Read the Markdown file
+  const filePath = path.join(__dirname, "../resources/sdkman.md");
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading markdown file');
+    }
+    // Convert Markdown to HTML
+    const htmlContent = marked(data);
+    // Render EJS template
+    res.render('template', { content: htmlContent });
+  });
+});
+
 export default router;
